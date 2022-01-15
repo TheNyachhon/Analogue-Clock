b41dc069c18a21046cd3d97b2bed7d8082b02d8f
Rujin Shrestha <63194470+TheNyachhon@users.noreply.github.com>
2022-01-15 13:20:24 +0530

    graduations added 🕤 + minor var fix

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const curMinutes = d.getMinutes()
 let minutesDeg = (curMinutes)*6 + (curSeconds*0.1)
 minute.style.transform = `rotate(${minutesDeg}deg)`
 //Hours
-const curHours = d.getHours()
-curHours > 12 ? curHours=-12 : curHours //chaning to 12 hour format
+let curHours = d.getHours()
+curHours > 12 ? curHours-=12 : curHours //chaning to 12 hour format
 let hoursDeg = curHours*30 + (curSeconds+curMinutes*60)*(1/120)
 hour.style.transform = `rotate(${hoursDeg}deg)`
 
@@ -27,7 +27,7 @@ setInterval(()=>{
     hour.style.transform = `rotate(${hoursDeg}deg)`;
 },1000)
 
-//Github icon
+//Github Link icon
 const icon = document.querySelector('.fa-github')
 const profile = document.querySelector('#github>p')
 icon.addEventListener('mouseenter', () => {
